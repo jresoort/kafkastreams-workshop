1863377f5afdbf037f4b871815c58eee03f76147
Jeroen Resoort <jeroen@resoort.net>
2018-06-13 06:39:26 +0200

    remove unused imports

--- kafkastreams/src/main/java/com/jdriven/kafkaworkshop/SensorController.java
+++ kafkastreams/src/main/java/com/jdriven/kafkaworkshop/SensorController.java
@@ -1,19 +1,12 @@
 package com.jdriven.kafkaworkshop;
 
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 @Controller
 public class SensorController {
